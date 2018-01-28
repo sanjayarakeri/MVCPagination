fd228eb50ebc3919fa81f9f8fad51561e382cb85
sanjayarakeri <sanjay05795@gmail.com>
2018-01-28 10:47:56 +0530

    MVC Pagination

---
+++ mvcpagination/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcpagination.MvcApplication" Language="C#" %>
---
+++ mvcpagination/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcpagination.Models
+{
+
+    public class Student
+    {
+        public int serialNo { get; set; }
+        public string USN { get; set; }
+        public string studentName { get; set; }
+        public string branch { get; set; }
+
+
+        public static List<Student> ListOfStudents()
+        {
+            List<Student> students = new List<Student>();
+
+            string[] branches = { "CSE", "ISE", "ECE", "EEE", "CE" };
+                Random rnd = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                students.Add(new Student() { serialNo = i, USN = "2BU10CS40" + i, branch = branches[rnd.Next(1, 5)], studentName = "Name" + i });
+            }
+
+            return students;
+        }
+
+    }
+
+}
